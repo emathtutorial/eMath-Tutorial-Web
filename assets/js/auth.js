16dffc5e97bc28f4fecdf8bf7cfc9b42432ecd1f
emathtutorial <emathtutorialhelp@gmail.com>
2022-02-16 05:28:48 +0530

    gallery has been implemented :)

---
+++ assets/js/auth.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBoeDYK6ME5Ef7-erp8_72G9ah2vc1v3iE",
+    authDomain: "website-691c4.firebaseapp.com",
+    databaseURL: "https://website-691c4-default-rtdb.firebaseio.com/",
+    projectId: "website-691c4",
+    storageBucket: "website-691c4.appspot.com",
+    messagingSenderId: "1083380707980",
+    appId: "1:1083380707980:web:048a7f49bba73ce20681a9",
+    measurementId: "G-VESL5NK3YY"
+  };
+  firebase.initializeApp(firebaseConfig);
